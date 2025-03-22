bc048c2f419575052b3909f882dd780c9ca94dec
tri-susilo <trisusilo.developer@gmail.com>
2025-03-22 15:34:28 +0700

    [ADD] script build and push

---
+++ build_push_image.sh
@@ -0,0 +1,4 @@
+# create docker image dari dockerfile dengan nama image "item-app" dan tag v1
+# melihat daftar image di lokal "docker images"
+# mengubah nama image agar sesau idengan format github packages
+# upload image ke docker hub
